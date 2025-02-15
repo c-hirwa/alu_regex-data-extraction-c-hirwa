940fb3bcb820d09b89cb3e0f526de804dbbc5512
c-hirwa <c.hirwa@alustudent.com>
2025-02-15 23:14:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,9 @@ def load_sample_text(filename):
         return file.read()
     
 #Regex function to filter needed elements
+# 1. Extracting Email Addresses
+def extract_Emails(Emails):
+    Emails_pattern = r'\b[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}\b'
+    return re.findall(Emails_pattern, Emails)
+
+# 2. Extracting URLs
